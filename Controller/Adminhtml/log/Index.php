29aed137c079a929edc0140b5736435619a4e522
Niels Gongoll <ngongoll@highdigital.de>
2020-03-10 16:27:40 +0100

    logging grid

--- Controller/Adminhtml/log/Index.php
+++ Controller/Adminhtml/log/Index.php
@@ -1,28 +1,39 @@
 <?php
+
 namespace Magento\BilliePaymentMethod\Controller\Adminhtml\Log;
-use Magento\Backend\App\Action;
+
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
 
 class Index extends \Magento\Backend\App\Action
 {
-    protected $resultPageFactory = false;
 
-    public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
-    )
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(Context $context, PageFactory $resultPageFactory)
     {
-        parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
     }
 
+    /**
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
     public function execute()
     {
-
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
         $resultPage = $this->resultPageFactory->create();
-        $resultPage->getConfig()->getTitle()->prepend((__('Billie Transaction Logs')));
-
+        $resultPage->setActiveMenu('Magento_BilliePaymentMethod::index')
+            ->getConfig()->getTitle()->prepend(__('Faqs'));
         return $resultPage;
     }
-
 
 }
