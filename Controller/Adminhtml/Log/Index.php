49d3e29e7ab8ce0361055cda7b8dcb49e2237078
ngongoll <ngongoll@welance.de>
2020-04-28 00:05:22 +0200

    add capital folder

---
+++ Controller/Adminhtml/Log/Index.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Magento\BilliePaymentMethod\Controller\Adminhtml\Log;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Magento_BilliePaymentMethod::log';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+x
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Magento_BilliePaymentMethod::log');
+        $resultPage->getConfig()->getTitle()->set((__('Billie Logs')));
+        return $resultPage;
+    }
+
+}
