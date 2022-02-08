1b603b5d07f0d062f87b43deb3d997f3e59afe8e
Frederik Rommel <frederik.rommel@webidea24.de>
2022-02-08 22:41:24 +0100

    remove code which has overridden magento default functionalities

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,15 +1,7 @@
 var config = {
     map: {
-
         '*': {
-
-            shoppingCart:           'Magento_Checkout/js/shopping-cart',
-            sidebar:                'Magento_Checkout/js/sidebar',
-            checkoutLoader:         'Magento_Checkout/js/checkout-loader',
-            checkoutData:           'Magento_Checkout/js/checkout-data',
-            proceedToCheckout:      'Magento_Checkout/js/proceed-to-checkout',
-            billiePayment:          'Billiepayment_BilliePaymentMethod/js/billie-widget'
-
+            billiePayment: 'Billiepayment_BilliePaymentMethod/js/billie-widget'
         }
     },
 };
