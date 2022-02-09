c0053444a47b1f0625aa879b4b5bae4ab02f60a4
Frederik Rommel <frederik.rommel@webidea24.de>
2022-02-09 02:34:46 +0100

    BILLMAGE-13: fix: loading additional item data. loading of data was has decreased the performance in the cart and caused issued if no category was given

--- Model/DefaultConfigProvider.php
+++ Model/DefaultConfigProvider.php
@@ -1,42 +1,89 @@
 <?php
+
 namespace Billiepayment\BilliePaymentMethod\Model;
 
+use Magento\Catalog\Model\ResourceModel\AbstractResource;
+use Magento\Catalog\Model\ResourceModel\Category;
+use Magento\Catalog\Model\ResourceModel\Product;
 use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Store\Model\StoreManagerInterface;
 
 class DefaultConfigProvider
 {
+    /**
+     * @var Category
+     */
+    private $categoryResourceModel;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var Product
+     */
+    private $productResourceModel;
+
     /**
      * @var CheckoutSession
      */
     protected $checkoutSession;
 
-    protected $_categoryFactory;
-    /**
-     * Constructor
-     *
-     * @param CheckoutSession $checkoutSession
-     */
     public function __construct(
-        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
-        CheckoutSession $checkoutSession
-    ) {
-        $this->_categoryFactory = $categoryFactory;
+        Category              $categoryResourceModel,
+        Product               $productResourceModel,
+        StoreManagerInterface $storeManager,
+        CheckoutSession       $checkoutSession
+    )
+    {
         $this->checkoutSession = $checkoutSession;
+        $this->categoryResourceModel = $categoryResourceModel;
+        $this->productResourceModel = $productResourceModel;
+        $this->storeManager = $storeManager;
     }
 
     public function afterGetConfig(
         \Magento\Checkout\Model\DefaultConfigProvider $subject,
-        array $result
-    ) {
+        array                                         $result
+    )
+    {
         $items = $result['totalsData']['items'];
         foreach ($items as $index => $item) {
             $quoteItem = $this->checkoutSession->getQuote()->getItemById($item['item_id']);
-            $result['quoteItemData'][$index]['manufacturer'] = $quoteItem->getProduct()->getAttributeText('manufacturer');
-            $result['quoteItemData'][$index]['description'] = $quoteItem->getProduct()->getDescription();
-            $categoryIds = $quoteItem->getProduct()->getCategoryIds();
-            $category = $this->_categoryFactory->create()->load($categoryIds[0]);
-            $result['quoteItemData'][$index]['category'] = $category->getName();
+            $product = $quoteItem ? $quoteItem->getProduct() : null;
+            if ($product) {
+
+                $categoryIds = $product->getCategoryIds();
+                $categoryName = isset($categoryIds[0]) ? $this->getAttributeValue($this->categoryResourceModel, $categoryIds[0], 'name') : null;
+
+                $result['quoteItemData'][$index]['billie_additional_data'] = [
+                    'manufacturer' => $this->getAttributeValue($this->productResourceModel, $product->getId(), 'manufacturer'),
+                    'description' => $this->getAttributeValue($this->productResourceModel, $product->getId(), 'description'),
+                    'category' => $categoryName,
+                ];
+            }
         }
+
         return $result;
+    }
+
+    private function getAttributeValue(AbstractResource $sourceModel, $entityId, $code)
+    {
+        $storeId = $this->storeManager->getStore()->getId();
+        $value = $sourceModel->getAttributeRawValue($entityId, $code, $storeId);
+        $value = $value !== false ? $value : null;
+        if (is_array($value)) {
+            $value = count($value) === 0 ? null : array_shift($value);
+        }
+
+        if ($value !== null) {
+            $attributeModel = $sourceModel->getAttribute($code);
+            if ($attributeModel && $attributeModel->usesSource()) {
+                $value = $attributeModel->getSource()->getOptionText($value);
+            }
+        }
+
+        return is_string($value) ? strip_tags($value) : null;
     }
 }
